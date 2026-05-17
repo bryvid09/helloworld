9ba3ee885d642127cda77280a4db3f8410b70d23
btTHN <brayan.tipan@mylighthouse.com>
2026-05-17 23:47:15 +0200

    rollback calc.py

--- app/calc.py
+++ app/calc.py
@@ -21,7 +21,7 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise                                                TypeError("Division by zero is not possible")
+            raise TypeError("Division by zero is not possible")
 
         return x / y
 
@@ -32,6 +32,7 @@ class Calculator:
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
